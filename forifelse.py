6f81d9de062df0a5f03d8728f13400fc30536136
puneethprog9 <puneethreddyb@gmail.com>
2017-03-20 17:10:11 +0000

    for if else

---
+++ forifelse.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+lists=['puneeth','madhu','siva','ramu']
+
+for i in lists:
+    if i=='puneeth':
+       print('Aws Cloud Infrastructure Engineer')
+    elif i=='madhu':
+       print('This is sql admin')
+    else:
+       print('nothing matches')
